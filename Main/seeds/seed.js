58c645003d93d7a38cda0e6414f43f6d1b0a626d
tommysoprano <thomasjaymaly@gmail.com>
2023-01-14 11:32:28 -0500

    added a reviews seed

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -4,6 +4,9 @@ const { User, Project } = require('../models');
 const userData = require('./userData.json');
 const projectData = require('./projectData.json');
 
+const reviews = require('./reviews.json');
+
+
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 

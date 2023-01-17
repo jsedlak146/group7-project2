b28943f4e00da00654f592696ceef19aadac82c4
Jonathan Plaras <jonplaras@gmail.com>
2023-01-17 11:52:03 -0500

    added graph handlebar and graph route to get data from quitplan

---
+++ controllers/api/graphRoute.js
@@ -0,0 +1,52 @@
+const { QuitPlan } = require("../../models");
+const Chart = require('chart.js');
+
+
+app.get('/api/Users/:id', withAuth, (req, res) => {
+    QuitPlan.findByPk(req.params.id).then(user => {
+        const howManyCigs = user.howManyCigs;
+        const cigPrice = user.cigPrice;
+
+        // chart.js needs a "canvas" to render the graph
+        // <canvas id='myChart'> </canvas> to wrap the graph
+        const ctx = document.getElementById('myChart').getContext('2d');
+        // this is where you create a graph with the data
+        const chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                // x and y axis?
+                labels: ['Cigarettes per week', 'Money spent per week'],
+                datasets: [{
+                    // label of the data set / graph
+                    label: 'Weekly use and cost of cigarettes',
+                    data: [howManyCigs, cigPrice],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2',
+                        'rgba(54, 162, 235, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        // this is to render the data to the handlebar
+        res.render('graph-card'), {
+            howManyCigs: howManyCigs,
+            cigPrice: cigPrice,
+            chart: chart.toBase64Image()
+        }
+
+    })
+
+})
+

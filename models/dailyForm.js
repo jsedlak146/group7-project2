2eaaeaae8cdefc3f6756884a7f43dba53c423662
jsedlak146 <jssedlak146@gmail.com>
2023-01-17 10:57:50 -0500

    added some more information to models, made seeds folder to build out

---
+++ models/dailyForm.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class DailyForm extends Model {}
+
+DailyForm.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.fn('NOW')
+        },
+        howManyCigs: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        cravings: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: false,
+        },
+        mood: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        daysWithoutCigs: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        triggers: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'daily_form',
+    }
+);
+
+module.exports = DailyForm;
---
+++ models/userStories.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserStories extends Model {}
+
+UserStories.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.fn('NOW')
+        },
+        daysWithoutCigs: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        story: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user_story',
+    }
+);
+
+module.exports = UserStories;

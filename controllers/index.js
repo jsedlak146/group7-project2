55ee54727c2ffa27fcc5be43830bddc165f94f6b
Jonathan Plaras <jonplaras@gmail.com>
2023-01-18 14:30:48 -0500

    updated code to make sure routes work, still needs debugging

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const browserRoutes = require('./browserRoutes');
 
-router.use('/', browserRoutes);
 router.use('/api', apiRoutes);
+router.use('/', browserRoutes);
 
 module.exports = router;

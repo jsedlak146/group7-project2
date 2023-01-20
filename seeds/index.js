43ca9273eba4eabae3d69349a2316f81f8b6e593
Jonathan Plaras <jonplaras@gmail.com>
2023-01-20 12:30:58 -0500

    added data to seed and fixed profile.handlebars to show graph, now shows data for 5 day span

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,19 @@
 const sequelize = require('../config/connection');
-const { UserStories} = require('../models');
-
+const { UserStories, User, DailyForm } = require('../models');
+const dailyFormData = require('./dailyFormSeeds.json');
 const userStoriesData = require('./userStorySeeds.json');
+const userData = require('./userSeeds.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
     await UserStories.bulkCreate(userStoriesData)
     console.log("User Stories Seeded")
+    
+    await User.bulkCreate(userData)
+    console.log("users created")
+    await DailyForm.bulkCreate(dailyFormData)
+    console.log("user daily forms seeded!!!")
+
 
 
   process.exit(0);
